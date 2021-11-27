feae1cfa74309cbdc4e3eef96c3341649b9959dc
Lucas Maia <lucas.amelo@b2wdigital.com>
2021-11-27 15:57:45 -0300

    test:create test for deleteMessage and recoverMessage services

---
+++ internal/app/server/domain/service/deleteMessage_test.go
@@ -0,0 +1,182 @@
+package service
+
+import (
+	"container/list"
+	"context"
+	"errors"
+	"github.com/maiaaraujo5/udp-chat/internal/app/server/domain/model"
+	"github.com/maiaaraujo5/udp-chat/internal/app/server/domain/repository/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"reflect"
+	"testing"
+)
+
+type DeleteMessageSuite struct {
+	suite.Suite
+}
+
+func TestDeleteMessageSuite(t *testing.T) {
+	suite.Run(t, new(DeleteMessageSuite))
+}
+
+func (s *DeleteMessageSuite) TestNewDelete() {
+	type args struct {
+		repository *mocks.Repository
+	}
+	tests := []struct {
+		name string
+		args args
+		want DeleteMessage
+	}{
+		{
+			name: "should build NewDelete successfully",
+			args: args{
+				repository: new(mocks.Repository),
+			},
+			want: &DeleteImpl{
+				repository: new(mocks.Repository),
+			},
+		},
+	}
+	for _, tt := range tests {
+
+		s.Run(tt.name, func() {
+			got := NewDelete(tt.args.repository)
+			s.Assert().True(reflect.DeepEqual(got, tt.want), "NewDelete() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+func (s *DeleteMessageSuite) TestDeleteImpl_Execute() {
+	type fields struct {
+		repository *mocks.Repository
+	}
+	type args struct {
+		parentCtx context.Context
+		message   *model.Message
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+		mock    func(repository *mocks.Repository)
+	}{
+		{
+			name: "should successfully delete one message",
+			fields: fields{
+				repository: new(mocks.Repository),
+			},
+			args: args{
+				parentCtx: context.Background(),
+				message: &model.Message{
+					ID:     "2",
+					UserID: "user_id",
+				},
+			},
+			wantErr: false,
+			mock: func(repository *mocks.Repository) {
+				repository.On("List", mock.Anything).Return(func() *list.List {
+					l := list.New()
+					l.PushBack(model.Message{ID: "1", UserID: mock.Anything, Message: mock.Anything})
+					l.PushBack(model.Message{ID: "2", UserID: "user_id", Message: mock.Anything})
+					l.PushBack(model.Message{ID: "3", UserID: mock.Anything, Message: mock.Anything})
+					return l
+				}(), nil).Once()
+
+				repository.On("SaveAll", mock.Anything, func() *list.List {
+					l := list.New()
+					l.PushBack(model.Message{ID: "1", UserID: mock.Anything, Message: mock.Anything})
+					l.PushBack(model.Message{ID: "3", UserID: mock.Anything, Message: mock.Anything})
+					return l
+				}()).Return(nil).Once()
+			},
+		},
+		{
+			name: "should not delete a message that does not belong to the user",
+			fields: fields{
+				repository: new(mocks.Repository),
+			},
+			args: args{
+				parentCtx: context.Background(),
+				message: &model.Message{
+					ID:     "1",
+					UserID: "user_id",
+				},
+			},
+			wantErr: false,
+			mock: func(repository *mocks.Repository) {
+				repository.On("List", mock.Anything).Return(func() *list.List {
+					l := list.New()
+					l.PushBack(model.Message{ID: "1", UserID: mock.Anything, Message: mock.Anything})
+					l.PushBack(model.Message{ID: "2", UserID: "user_id", Message: mock.Anything})
+					l.PushBack(model.Message{ID: "3", UserID: mock.Anything, Message: mock.Anything})
+					return l
+				}(), nil).Once()
+			},
+		},
+		{
+			name: "should return error when repository.List returns error",
+			fields: fields{
+				repository: new(mocks.Repository),
+			},
+			args: args{
+				parentCtx: context.Background(),
+				message: &model.Message{
+					ID:     mock.Anything,
+					UserID: mock.Anything,
+				},
+			},
+			wantErr: true,
+			mock: func(repository *mocks.Repository) {
+				repository.On("List", mock.Anything).Return(nil, errors.New("error to recover messages"))
+			},
+		},
+		{
+			name: "should return error when repository.SaveAll returns error",
+			fields: fields{
+				repository: new(mocks.Repository),
+			},
+			args: args{
+				parentCtx: context.Background(),
+				message: &model.Message{
+					ID:     "2",
+					UserID: "user_id",
+				},
+			},
+			wantErr: true,
+			mock: func(repository *mocks.Repository) {
+				repository.On("List", mock.Anything).Return(func() *list.List {
+					l := list.New()
+					l.PushBack(model.Message{ID: "1", UserID: mock.Anything, Message: mock.Anything})
+					l.PushBack(model.Message{ID: "2", UserID: "user_id", Message: mock.Anything})
+					l.PushBack(model.Message{ID: "3", UserID: mock.Anything, Message: mock.Anything})
+					return l
+				}(), nil).Once()
+
+				repository.On("SaveAll", mock.Anything, func() *list.List {
+					l := list.New()
+					l.PushBack(model.Message{ID: "1", UserID: mock.Anything, Message: mock.Anything})
+					l.PushBack(model.Message{ID: "3", UserID: mock.Anything, Message: mock.Anything})
+					return l
+				}()).Return(errors.New("error to save messages")).Once()
+			},
+		},
+	}
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+
+			tt.mock(tt.fields.repository)
+
+			d := &DeleteImpl{
+				repository: tt.fields.repository,
+			}
+
+			err := d.Execute(tt.args.parentCtx, tt.args.message)
+			s.Assert().True((err != nil) == tt.wantErr, "Execute() error = %v, wantErr %v", err, tt.wantErr)
+
+			tt.fields.repository.AssertExpectations(s.T())
+		})
+	}
+}
---
+++ internal/app/server/domain/service/recoverMessages_test.go
@@ -0,0 +1,127 @@
+package service
+
+import (
+	"container/list"
+	"context"
+	"errors"
+	"github.com/maiaaraujo5/udp-chat/internal/app/server/domain/model"
+	"github.com/maiaaraujo5/udp-chat/internal/app/server/domain/repository/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"reflect"
+	"testing"
+)
+
+type RecoverMessageSuite struct {
+	suite.Suite
+}
+
+func TestRecoverMessageSuite(t *testing.T) {
+	suite.Run(t, new(RecoverMessageSuite))
+}
+
+func (s *RecoverMessageSuite) TestNewRecover() {
+	type args struct {
+		repository *mocks.Repository
+	}
+	tests := []struct {
+		name string
+		args args
+		want Recover
+	}{
+		{
+			name: "should build NewRecover successfully",
+			args: args{
+				repository: new(mocks.Repository),
+			},
+			want: &RecoverImpl{
+				repository: new(mocks.Repository),
+			},
+		},
+	}
+	for _, tt := range tests {
+
+		s.Run(tt.name, func() {
+			got := NewRecover(tt.args.repository)
+			s.Assert().True(reflect.DeepEqual(got, tt.want), "NewRecover() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+func (s *RecoverMessageSuite) TestRecoverImpl_Execute() {
+	type fields struct {
+		repository *mocks.Repository
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []model.Message
+		wantErr bool
+		mock    func(repository *mocks.Repository)
+	}{
+		{
+			name: "should successfully return all messages that returned from repository in order",
+			fields: fields{
+				repository: new(mocks.Repository),
+			},
+			args: args{
+				ctx: context.Background(),
+			},
+			want: []model.Message{
+				{
+					ID:      "1",
+					UserID:  mock.Anything,
+					Message: mock.Anything,
+				},
+				{
+					ID:      "2",
+					UserID:  mock.Anything,
+					Message: mock.Anything,
+				},
+			},
+			wantErr: false,
+			mock: func(repository *mocks.Repository) {
+				repository.On("List", mock.Anything).Return(func() *list.List {
+					l := list.New()
+					l.PushBack(model.Message{ID: "1", UserID: mock.Anything, Message: mock.Anything})
+					l.PushBack(model.Message{ID: "2", UserID: mock.Anything, Message: mock.Anything})
+					return l
+				}(), nil).Once()
+			},
+		},
+		{
+			name: "should return error when repository return error",
+			fields: fields{
+				repository: new(mocks.Repository),
+			},
+			args: args{
+				ctx: context.Background(),
+			},
+			want:    nil,
+			wantErr: true,
+			mock: func(repository *mocks.Repository) {
+				repository.On("List", mock.Anything).Return(nil, errors.New("error to return messages"))
+			},
+		},
+	}
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+
+			tt.mock(tt.fields.repository)
+
+			r := &RecoverImpl{
+				repository: tt.fields.repository,
+			}
+
+			got, err := r.Execute(tt.args.ctx)
+			s.Assert().True((err != nil) == tt.wantErr, "Execute() error = %v, wantErr %v", err, tt.wantErr)
+			s.Assert().True(reflect.DeepEqual(got, tt.want), "Execute() = %v, want %v", got, tt.want)
+
+			tt.fields.repository.AssertExpectations(s.T())
+		})
+	}
+}
